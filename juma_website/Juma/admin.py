eaa1fdcf48f46860aa4f74304c97a3e0636976e7
meme cho <thiagoomalap@gmail.com>
2025-10-13 10:22:55 -0300

    Fix admin

--- juma_website/Juma/admin.py
+++ juma_website/Juma/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Producto, ImagenProducto, Carrito, ItemCarrito
 
-# Register your models here.
+
+class ImagenProductoInline(admin.TabularInline):  # o StackedInline si querés en columnas
+    model = ImagenProducto
+    extra = 1  # cantidad de formularios vacíos listos para cargar
+
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio_venta', 'stock')
+    search_fields = ('nombre',)
+    list_filter = ('precio_venta',)
+    ordering = ('nombre',)
+    inlines = [ImagenProductoInline]  # 👈 esta línea es la clave
+
+
+@admin.register(ImagenProducto)
+class ImagenProductoAdmin(admin.ModelAdmin):
+    list_display = ('producto', 'principal')
+    list_filter = ('principal',)
+
+
+@admin.register(Carrito)
+class CarritoAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'creado_en', 'activo')
+    list_filter = ('activo', 'creado_en')
+
+
+@admin.register(ItemCarrito)
+class ItemCarritoAdmin(admin.ModelAdmin):
+    list_display = ('carrito', 'producto', 'cantidad')
